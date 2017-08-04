999ee3c602ae94758e608ff4475e9da38d3896bc
J Mezart <noxilloux@gmail.com>
2017-08-04 10:57:15 +0000

    Installed Devise gem

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'noxilloux@yahoo.com'
+  default to: 'noxilloux@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'superduperdevmatch.herokuapp.com',
+  :domain         => 'powerful-stream-70805.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
